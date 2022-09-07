7a08238008fac91699ad998b0e7cd161d4c3a3ad
mrunalinidighe <93102379+mrunalinidighe@users.noreply.github.com>
2022-09-07 15:18:32 -0700

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,27 @@
-class BinarySearch { 
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :  no
+
+public class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+    	if (r>=l) {
+    		//Write your code here
+        	int mid = l+(r-l)/2;
+        	if (arr[mid]==x) {
+    			return mid;
+    		}
+        	if (arr[mid]>x) {
+    			return binarySearch(arr, l, mid-1, x);
+    		}
+        	//else element can be present in the right subarry
+        	return binarySearch(arr, mid+1, r, x);
+		}
+        // if nothing is found 
+    	return -1;
+
     } 
   
     // Driver method to test above 
@@ -18,4 +37,4 @@ class BinarySearch {
         else
             System.out.println("Element found at index " + result); 
     } 
-} 
+}
